1ceb9caaaa65c55e25c379bf008fd5cbf786bf52
Gonzalo Boquete <gonzaboquete@hotmail.com>
2024-05-30 09:58:22 -0300

    Correccion imports reportes

--- BD2Grupo05TP2024/src/main/java/com/api/Reportes.java
+++ BD2Grupo05TP2024/src/main/java/com/api/Reportes.java
@@ -9,7 +9,6 @@ import org.bson.Document;
 import com.api.dao.VentaDAO;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
 public class Reportes {
