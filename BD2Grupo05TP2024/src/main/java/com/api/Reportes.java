64bd85cb37e4ef544cebea77c68b3b6269fb400f
Gonzalo Boquete <gonzaboquete@hotmail.com>
2024-05-30 09:57:42 -0300

    Generacion de reportes

--- BD2Grupo05TP2024/src/main/java/com/api/Reportes.java
+++ BD2Grupo05TP2024/src/main/java/com/api/Reportes.java
@@ -1,9 +1,12 @@
 package com.api;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 import org.bson.Document;
 
+import com.api.dao.VentaDAO;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
@@ -12,16 +15,42 @@ import com.mongodb.client.MongoDatabase;
 public class Reportes {
 
 	public static void main(String[] args) {
-		LocalDate fechaDesde = LocalDate.of(2024, 1, 1); // Ejemplo: fecha desde
-		LocalDate fechaHasta = LocalDate.of(2024, 5, 31); // Ejemplo: fecha hasta
-
-		generarReporte(fechaDesde, fechaHasta);
-	}
-
-	public static void generarReporte(LocalDate fechaDesde, LocalDate fechaHasta) {
 		MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
 		MongoDatabase database = mongoClient.getDatabase("BD2Grupo05TP2024");
-		MongoCollection<Document> collection = database.getCollection("Venta");
+		VentaDAO ventaDAO = new VentaDAO(database);
+
+		LocalDate fechaDesde = LocalDate.parse("2023-01-01", DateTimeFormatter.ISO_DATE);
+		LocalDate fechaHasta = LocalDate.parse("2024-12-31", DateTimeFormatter.ISO_DATE);
+
+		List<Document> totalVentasTodaCadena = ventaDAO.totalVentasTodaCadena(fechaDesde, fechaHasta);
+		System.out.println("1.1 Total ventas en toda la cadena: " + totalVentasTodaCadena);
+
+		List<Document> totalVentasPorSucursal = ventaDAO.totalVentasPorSucursal(fechaDesde, fechaHasta);
+		System.out.println("1.2 Total ventas por sucursal: " + totalVentasPorSucursal);
+
+		List<Document> totalVentasPorObraSocial = ventaDAO.totalVentasPorObraSocial(fechaDesde, fechaHasta);
+		System.out.println("2. Total ventas por obra social: " + totalVentasPorObraSocial);
+
+		List<Document> totalCobranzaTodaCadena = ventaDAO.totalCobranzaTodaCadena(fechaDesde, fechaHasta);
+		System.out.println("3.1 Total cobranza en toda la cadena: " + totalCobranzaTodaCadena);
+
+		List<Document> totalCobranzaPorSucursal = ventaDAO.totalCobranzaPorSucursal(fechaDesde, fechaHasta);
+		System.out.println("3.2. Total cobranza por sucursal: " + totalCobranzaPorSucursal);
+
+		List<Document> ventasPorTipoProducto = ventaDAO.ventasPorTipoProducto(fechaDesde, fechaHasta);
+		System.out.println("4. Total ventas por tipo de producto: " + ventasPorTipoProducto);
+
+		List<Document> rankingMontoVendido = ventaDAO.rankingMontoVendido();
+		System.out.println("5. Ranking monto vendido: " + rankingMontoVendido);
+
+		List<Document> rankingCantidadProductosVendidos = ventaDAO.rankingCantidadProductosVendidos();
+		System.out.println("6. Ranking cantidad productos vendidos: " + rankingCantidadProductosVendidos);
+
+		List<Document> rankingComprasPorCliente = ventaDAO.rankingComprasPorCliente();
+		System.out.println("7. Ranking compras por cliente: " + rankingComprasPorCliente);
+
+		List<Document> rankingComprasPorClienteYSucursal = ventaDAO.rankingComprasPorClienteYSucursal();
+		System.out.println("8. Ranking compras por cliente y sucursal: " + rankingComprasPorClienteYSucursal);
 
 		mongoClient.close();
 	}
--- BD2Grupo05TP2024/src/main/java/com/api/dao/VentaDAO.java
+++ BD2Grupo05TP2024/src/main/java/com/api/dao/VentaDAO.java
@@ -1,14 +1,22 @@
 package com.api.dao;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.bson.Document;
+import org.bson.conversions.Bson;
 
 import com.api.model.Venta;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Accumulators;
+import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Sorts;
 
 public class VentaDAO {
 	private MongoCollection<Document> collection;
@@ -31,11 +39,128 @@ public class VentaDAO {
 	}
 
 	public Venta getVenta(int nroTicket) {
-		Document filtro = new Document("nroTicket", nroTicket); // creo un filtro para buscar por _id
-		Document ventaDoc = collection.find(filtro).first(); // busco el documento en la colección
+		Document filtro = new Document("nroTicket", nroTicket); // Creo un filtro para buscar por _id
+		Document ventaDoc = collection.find(filtro).first(); // Busco el documento en la colección
 		if (ventaDoc != null) {
 			return gson.fromJson(ventaDoc.toJson(), Venta.class);
 		}
 		return null;
+	}
+
+	public List<Document> totalVentasTodaCadena(LocalDate fechaDesde, LocalDate fechaHasta) {
+		// Filtro por fecha
+		Bson match = Aggregates.match(
+				Filters.and(Filters.gte("fecha", fechaDesde.toString()), Filters.lte("fecha", fechaHasta.toString())));
+		//Agrupo por todos los registros obtenidos bajo una sola variable "totalVentas"
+		Bson group = Aggregates.group(null, Accumulators.sum("totalVentas", 1));
+		//Se formula una pipeline a traves de los filtros que acabamos de crear
+		List<Bson> pipeline = Arrays.asList(match, group);
+		return collection.aggregate(pipeline).into(new ArrayList<>());
+	}
+
+	public List<Document> totalVentasPorSucursal(LocalDate fechaDesde, LocalDate fechaHasta) {
+		// Filtro por fecha
+		Bson match = Aggregates.match(
+				Filters.and(Filters.gte("fecha", fechaDesde.toString()), Filters.lte("fecha", fechaHasta.toString())));
+		//Agrupo por idSucursal bajo una sola variable "totalVentas"
+		Bson group = Aggregates.group("$atendidoPor.sucursal.idSucursal", Accumulators.sum("totalVentas", 1));
+		//Se formula una pipeline con los filtros que acabamos de crear
+		List<Bson> pipeline = Arrays.asList(match, group);
+		return collection.aggregate(pipeline).into(new ArrayList<>());
+	}
+
+	public List<Document> totalVentasPorObraSocial(LocalDate fechaDesde, LocalDate fechaHasta) {
+		// Filtro por fecha
+		Bson match = Aggregates.match(
+				Filters.and(Filters.gte("fecha", fechaDesde.toString()), Filters.lte("fecha", fechaHasta.toString())));
+		//Agrupo por obra social del cliente bajo una sola variable "totalVentas"
+		Bson group = Aggregates.group("$cliente.obraSocial", Accumulators.sum("totalVentas", 1));
+		//Se formula una pipeline con los filtros que acabamos de crear
+		List<Bson> pipeline = Arrays.asList(match, group);
+		return collection.aggregate(pipeline).into(new ArrayList<>());
+	}
+
+	public List<Document> totalCobranzaTodaCadena(LocalDate fechaDesde, LocalDate fechaHasta) {
+		// Filtro por fecha
+		Bson match = Aggregates.match(
+				Filters.and(Filters.gte("fecha", fechaDesde.toString()), Filters.lte("fecha", fechaHasta.toString())));
+		//Acumulo el valor de "valorVenta" bajo "totalCobranza"
+		Bson group = Aggregates.group(null, Accumulators.sum("totalCobranza", "$valorVenta"));
+		//Se formula una pipeline con los filtros que acabamos de crear
+		List<Bson> pipeline = Arrays.asList(match, group);
+		return collection.aggregate(pipeline).into(new ArrayList<>());
+	}
+
+	public List<Document> totalCobranzaPorSucursal(LocalDate fechaDesde, LocalDate fechaHasta) {
+		// Filtro por fecha
+		Bson match = Aggregates.match(
+				Filters.and(Filters.gte("fecha", fechaDesde.toString()), Filters.lte("fecha", fechaHasta.toString())));
+		//Agrupo por idSucursal y acumulo el valor de "valorVenta" bajo "totalCobranza"
+		Bson group = Aggregates.group("$atendidoPor.sucursal.idSucursal",
+				Accumulators.sum("totalCobranza", "$valorVenta"));
+		//Se formula una pipeline con los filtros que acabamos de crear
+		List<Bson> pipeline = Arrays.asList(match, group);
+		return collection.aggregate(pipeline).into(new ArrayList<>());
+	}
+
+	public List<Document> ventasPorTipoProducto(LocalDate fechaDesde, LocalDate fechaHasta) {
+		// Filtro por fecha
+		Bson match = Aggregates.match(
+				Filters.and(Filters.gte("fecha", fechaDesde.toString()), Filters.lte("fecha", fechaHasta.toString())));
+		//accedo al objeto "detallesVenta" dentro de "Venta"
+		Bson unwind = Aggregates.unwind("$detallesVenta");
+		//Agrupo por tipo de producto y acumulo las cantidades de "detalleVenta" bajo "totalVentas"
+		Bson group = Aggregates.group("$detallesVenta.producto.tipo",
+				Accumulators.sum("totalVentas", "$detallesVenta.cantidad"));
+		//Se formula una pipeline con los filtros que acabamos de crear
+		List<Bson> pipeline = Arrays.asList(match, unwind, group);
+		return collection.aggregate(pipeline).into(new ArrayList<>());
+	}
+
+	public List<Document> rankingMontoVendido() {
+		//accedo al objeto "detallesVenta" dentro de "Venta"
+		Bson unwind = Aggregates.unwind("$detallesVenta");
+		//Agrupo por codigo de producto e idSucursal y acumulo los totales de "detalleVenta" bajo "totalMonto"
+		Bson group = Aggregates.group(Arrays.asList("$detallesVenta.producto.codigo", "$sucursal.idSucursal"),
+				Accumulators.sum("totalMonto", "$detallesVenta.total"));
+		//Ordeno de mayor a menor por "totalMonto"
+		Bson sort = Aggregates.sort(Sorts.descending("totalMonto"));
+		//Se formula una pipeline con los filtros que acabamos de crear
+		List<Bson> pipeline = Arrays.asList(unwind, group, sort);
+		return collection.aggregate(pipeline).into(new ArrayList<>());
+	}
+
+	public List<Document> rankingCantidadProductosVendidos() {
+		//accedo al objeto "detallesVenta" dentro de "Venta"
+		Bson unwind = Aggregates.unwind("$detallesVenta");
+		//Agrupo por codigo de producto e idSucursal y acumulo las cantidades de "detalleVenta" bajo "totalCantidad"
+		Bson group = Aggregates.group(Arrays.asList("$detallesVenta.producto.codigo", "$sucursal.idSucursal"),
+				Accumulators.sum("totalCantidad", "$detallesVenta.cantidad"));
+		//Ordeno de mayor a menor por "totalCantidad"
+		Bson sort = Aggregates.sort(Sorts.descending("totalCantidad"));
+		//Se formula una pipeline con los filtros que acabamos de crear
+		List<Bson> pipeline = Arrays.asList(unwind, group, sort);
+		return collection.aggregate(pipeline).into(new ArrayList<>());
+	}
+
+	public List<Document> rankingComprasPorCliente() {
+		//Agrupo por DNI de cliente y acumulo los valores de "valorVenta" bajo "totalCompras"
+		Bson group = Aggregates.group("$cliente.DNI", Accumulators.sum("totalCompras", "$valorVenta"));
+		//Ordeno de mayor a menor por "totalCompras"
+		Bson sort = Aggregates.sort(Sorts.descending("totalCompras"));
+		//Se formula una pipeline con los filtros que acabamos de crear
+		List<Bson> pipeline = Arrays.asList(group, sort);
+		return collection.aggregate(pipeline).into(new ArrayList<>());
+	}
+
+	public List<Document> rankingComprasPorClienteYSucursal() {
+		//Agrupo por DNI de cliente e idSucursal y acumulo los valores de "valorVenta" bajo "totalCompras"
+		Bson group = Aggregates.group(Arrays.asList("$cliente.DNI", "$sucursal.idSucursal"),
+				Accumulators.sum("totalCompras", "$valorVenta"));
+		//Ordeno de mayor a menor por "totalCompras"
+		Bson sort = Aggregates.sort(Sorts.descending("totalCompras"));
+		//Se formula una pipeline con los filtros que acabamos de crear
+		List<Bson> pipeline = Arrays.asList(group, sort);
+		return collection.aggregate(pipeline).into(new ArrayList<>());
 	}
 }
